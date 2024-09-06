d8d07c065b030222deba03d0a96873e6f0112241
wolfwdavid <49036883+karur0su@users.noreply.github.com>
2024-09-06 16:09:44 -0400

    first commit

---
+++ math.js
@@ -0,0 +1,10 @@
+//Check if all numbers are divisible by 5. Cache the result in a variable.
+//Check if the first number is larger than the last. Cache the result in a variable.
+//Accomplish the following arithmetic chain:
+//Subtract the first number from the second number.
+//Multiply the result by the third number.
+//Find the remainder of dividing the result by the fourth number.
+//Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
+
+
+//Part 2: Practical Math
